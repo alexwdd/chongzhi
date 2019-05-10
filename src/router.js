f2d42aefae2a6267cb330a8087f248625560be7b
alexwdd <20779512@qq.com>
2019-05-10 17:30:06 +0800

    0510

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Success from './views/Success.vue'
+import Fail from './views/Fail.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/success/:order_no',
       name: 'success',
       component: Success
+    },
+    {
+      path: '/fail',
+      name: 'fail',
+      component: Fail
     }
   ]
 })
---
+++ src/views/Fail.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="wrap">
+        <div class="ok">
+            <van-icon name="cross" />
+            <p>支付失败</p>
+        </div>
+        <p style="text-align:center; padding:20px"><van-button type="default" @click="goHome">继续购买</van-button></p>
+	</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+	},
+    created(){	
+	},
+    methods: {
+        goHome(){
+            this.$router.push({path:'/'});
+        }
+    }
+};
+</script>
+
+<style scoped>
+.ok{background: #f7f7f7; text-align: center; padding: 40px 0}
+.ok i{background: #c00; color: #fff; display: block; width: 60px; height: 60px; line-height: 60px;font-size: 40px; margin: auto;border-radius: 50%}
+.ok p{margin-top: 20px;}
+</style>
